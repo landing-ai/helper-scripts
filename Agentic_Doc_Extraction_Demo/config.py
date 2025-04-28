5e1953ed6d80af5d2a4891e97f4a19dd484ec6dc
davidyspark <117950702+davidyspark@users.noreply.github.com>
2025-04-28 15:19:22 -0400

    Delete Agentic_Doc_Extraction_Demo directory

--- Agentic_Doc_Extraction_Demo/config.py
+++
@@ -1,8 +0,0 @@
-API_KEYS = {
-    # Add your Agentic Document Extraction API Key
-    "ade": "your_ade_api_key_here", 
-    # Add your VisionAgent API Key
-    "va": "your_va_api_key_here",
-    # Add your OpenAI Key
-    "openai": "your_openai_key_here"
-}
